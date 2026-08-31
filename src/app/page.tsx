383180564c6058ece988258ee5b3888ff21ad98d
Shashini Rathnayake <shashinirathnayake2111@gmail.com>
2026-08-31 22:44:05 +0530

    Change root path of the app folder

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Navbar from "@/src/components/Navbar";
+import { useState, useRef } from "react";
+
+export default function Home() {
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main></main>
+    </div>
+  );
+}
